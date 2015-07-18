4cff97eb68e9ece2e3a21d8174f1bcef16f45d55
tore.brede@gmail.com <tore.brede@gmail.com>
2015-07-18 11:24:37 +0200

    Adding pom-files

---
+++ rest/src/test/java/com/kjipo/rest/EndPointTest.java
@@ -0,0 +1,46 @@
+package com.kjipo.rest;
+
+import com.kjipo.data.DataProvider;
+import kjipo.com.kjipo.rest.ParameterEndpoints;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = {ParameterEndpoints.class, DataProvider.class})
+@WebAppConfiguration
+public class EndPointTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testRequest() throws Exception {
+        // TODO Finish test
+
+        mockMvc.perform(get("/parameters/Parameter1")
+                .param("start", "1000")
+                .param("stop", "1500"))
+                .andExpect(status().isOk());
+    }
+
+}
